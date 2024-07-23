c1d6675b3ace03008215f8d123e4abe499373fe6
Djtu75 <Gideonzim@gmail.com>
2024-07-23 10:51:35 -0500

    added some comments

--- Risk Java/participant/Logic.java
+++ Risk Java/participant/Logic.java
@@ -5,15 +5,15 @@ import util.*;
 public interface Logic {
 
     public void initialize(Game mygame);
-    public void beginTurn();
-    public void endTurn();
+    public void beginTurn(); //Method to tell player their turn is about to start. Intended to let player initialize variables and logic.
+    public void endTurn(); //Method to tell player their turn is about to end. Intended to let player clean up variables and logic.
 
-    public Object[] draftPhase(int numTroopstoPlace);
-    public Object[] attackPhase();
-    public Object[] movePhase();
+    public Object[] draftPhase(int numTroopstoPlace); //Asks player to place troops. Should return [int numTroopstoPlace, Province destination]
+    public Object[] attackPhase(); //Asks player to perform attack. Should return [int numTroopstoUse, Province attackingProvince, Province defendingProvince]
+    public Object[] movePhase(); //Asks player to move troops. Should return [int numTroopstoMove, Province source, Province destination]
 
-    public int moveAfterConquer(Province attackingProvince, Province defendingProvince);
-    public Object[] turnInCards(boolean required, int currentTroops);
+    public int moveAfterConquer(Province attackingProvince, Province defendingProvince); //Asks player how many troops to move into conquered territory. Should return [int numTroops]
+    public Object[] turnInCards(boolean required, int currentTroops); //Asks player to turn in set of cards. If required, must be [Card X, Card Y, Card Z]. If optional, any invalid input will count as passing on your turn.
 
     
 }
--- Risk Java/participant/PlayerLogic.java
+++ Risk Java/participant/PlayerLogic.java
@@ -11,20 +11,33 @@ public class PlayerLogic implements Logic {
         this.mygame = mygame;
     }
 
+    /**
+     * @return Method to tell player their turn is about to start. Intended to let player initialize variables and logic.
+     */
     public void beginTurn(){
         //Do setup logic
     }
 
+    /**
+     * @return //Method to tell player their turn is about to end. Intended to let player clean up variables and logic.
+     */
     public void endTurn(){
         //Do ending logic
     }
 
+    /**
+     * @return //Asks player to place troops. Should return [int numTroopstoPlace, Province destination]
+     */
     public Object[] draftPhase(int numTroopstoPlace){
         Province destination = new Province(null, null, 0);
         int placeXTroops = 1;
         Object[] returnArray = {placeXTroops, destination};
         return(returnArray);
     }
+
+    /**
+     * @return //Asks player to perform attack. Should return [int numTroopstoUse, Province attackingProvince, Province defendingProvince]
+     */
     public Object[] attackPhase(){
         int useXtroops = 3;
         Province attackingProvince = new Province(null, null, 0);
@@ -33,6 +46,10 @@ public class PlayerLogic implements Logic {
         return (returnArray);
 
     }
+
+    /**
+     * @return //Asks player to move troops. Should return [int numTroopstoMove, Province source, Province destination]
+     */
     public Object[] movePhase(){
         int moveXTroops = 1;
         Province source = new Province(null, null, 0);
@@ -41,11 +58,19 @@ public class PlayerLogic implements Logic {
         return (returnArray);
     }
 
+
+    /**
+     * @return //Asks player how many troops to move into conquered territory. Should return [int numTroops]
+     */
     public int moveAfterConquer(Province attackingProvince, Province defendingProvince){
         int transferXTroops = 1;
         return(transferXTroops);
 
     }
+
+    /**
+     * @return //Asks player to turn in set of cards. If required, must be [Card X, Card Y, Card Z]. If not required, any invalid input will count as passing on your turn.
+     */
     public Object[] turnInCards(boolean required, int currentTroops){
         if(required){
             Set<Card> mycards = mygame.getCardData(this);
