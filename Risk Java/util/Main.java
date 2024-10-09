f7aafa395fcfca33570850aecbbb06fdf5704962
Djtu75 <Gideonzim@gmail.com>
2024-10-09 15:25:53 -0500

    Profile rendering prototype

--- Risk Java/util/Main.java
+++ Risk Java/util/Main.java
@@ -24,12 +24,16 @@ public class Main {
 
         Player one = new Player("one", sl1);
         one.setColors(new Color(252, 186, 3), new Color(128, 252, 3));
+        one.setProfilePictureLink("https://waapple.org/wp-content/uploads/2021/06/Variety_Cosmic-Crisp-transparent-658x677.png");
         Player two = new Player("two", sl2);
         two.setColors(new Color(15, 252, 56), new Color(100, 160, 30));
+        two.setProfilePictureLink("https://i.guim.co.uk/img/media/b1c1caa029d6f186f9d6b3fabb7f02517eb9c33b/0_58_2528_1519/master/2528.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=22aae08708a226561bcdc42856a2bb18");
         Player three = new Player("three", sl3);
         three.setColors(new Color(0, 40, 160), new Color(25, 65, 200));
+        three.setProfilePictureLink("https://i.guim.co.uk/img/media/b1c1caa029d6f186f9d6b3fabb7f02517eb9c33b/0_58_2528_1519/master/2528.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=22aae08708a226561bcdc42856a2bb18");
         Player four = new Player("four", sl4);
         four.setColors(new Color(38, 186, 252), new Color(252, 100, 48));
+        four.setProfilePictureLink("https://i.guim.co.uk/img/media/b1c1caa029d6f186f9d6b3fabb7f02517eb9c33b/0_58_2528_1519/master/2528.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=22aae08708a226561bcdc42856a2bb18");
         Player[] players = {one, two, three, four};
         Province Alaska = new Province("Alaska", one, 0);
         Province Northwest_Territory = new Province("Northwest_Territory", one, 0);
